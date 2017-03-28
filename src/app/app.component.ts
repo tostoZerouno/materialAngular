39767e11c0619edcd26478fb6c110acaf380934e
tostoZerouno <tommaso.tosi92@outlook.it>
2017-03-28 11:23:42 +0200

    prima semplice app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  name = 'Angular';
+  text = "Clicca per cominciare!";
+  btnState=false;
+  icona="play_arrow";
+
+  onClick(){
+    this.btnState=!this.btnState;
+    this.text = this.btnState ? "Clicca per mettere in pausa!"  : "Clicca per ricominciare!";
+    this.icona=this.btnState ? "pause" : "play_arrow";
+    console.log(this.text);
+  }
+}
